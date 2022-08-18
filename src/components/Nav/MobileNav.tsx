890bc6cf4a6ef473a5cbddc2f28f5f9182c387f8
EnyelSequeira <enyelsequeira@hotmail.com>
2022-08-19 01:49:13 +0800

    feat(ui): adding modal on mobiles

--- src/components/Nav/MobileNav.tsx
+++ src/components/Nav/MobileNav.tsx
@@ -1,74 +1,94 @@
+import useToggleModal from '@/hooks/UI/useToggleModal';
+import useStore from '@/store/useStore';
 import { Popover, Transition } from '@headlessui/react';
 import { XIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import { Fragment } from 'react';
+import { SignIn, SignUp } from '@/components/Modals';
 import { navigation } from '.';
 
 const MobileNav = () => {
+  const isLoggedIn = useStore((state) => state.isLoggedIn);
+  const loginModal = useToggleModal();
+  const signUpModal = useToggleModal();
+
   return (
-    <Transition
-      as={Fragment}
-      enter="duration-150 ease-out"
-      enterFrom="opacity-0 scale-95"
-      enterTo="opacity-100 scale-100"
-      leave="duration-100 ease-in"
-      leaveFrom="opacity-100 scale-100"
-      leaveTo="opacity-0 scale-95"
-    >
-      <Popover.Panel
-        focus
-        className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
+    <>
+      <Transition
+        as={Fragment}
+        enter="duration-150 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
       >
-        <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-          <div className="px-5 pt-4 flex items-center justify-between">
-            <div>
-              <Image
-                className="h-8 w-auto sm:h-10"
-                src="/images/logo.svg"
-                width={32}
-                height={32}
-                alt="nav"
-              />
+        <Popover.Panel
+          focus
+          className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
+        >
+          <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <Image
+                  className="h-8 w-auto sm:h-10"
+                  src="/images/logo.svg"
+                  width={32}
+                  height={32}
+                  alt="nav"
+                />
+              </div>
+              <div className="-mr-2">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+                  <span className="sr-only">Close menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
             </div>
-            <div className="-mr-2">
-              <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
-                <span className="sr-only">Close menu</span>
-                <XIcon className="h-6 w-6" aria-hidden="true" />
-              </Popover.Button>
+            <div className="pt-5 pb-6">
+              <div className="px-2 space-y-1">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="mt-6 px-5">
+                {!isLoggedIn && (
+                  <button
+                    onClick={() => signUpModal.onToggle()}
+                    type="button"
+                    className="block text-center w-full py-3 px-4 rounded-md shadow bg-indigo-600 text-white font-medium hover:bg-indigo-700"
+                  >
+                    Start HERE
+                  </button>
+                )}
+              </div>
+              <div className="mt-6 px-5">
+                <p className="text-center text-base font-medium text-gray-500">
+                  Existing customer?{' '}
+                  {!isLoggedIn && (
+                    <button
+                      onClick={() => loginModal.onToggle()}
+                      type="button"
+                      className="text-gray-900 hover:underline"
+                    >
+                      Login
+                    </button>
+                  )}
+                </p>
+              </div>
             </div>
           </div>
-          <div className="pt-5 pb-6">
-            <div className="px-2 space-y-1">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-                >
-                  {item.name}
-                </a>
-              ))}
-            </div>
-            <div className="mt-6 px-5">
-              <button
-                type="button"
-                className="block text-center w-full py-3 px-4 rounded-md shadow bg-indigo-600 text-white font-medium hover:bg-indigo-700"
-              >
-                Start free trial
-              </button>
-            </div>
-            <div className="mt-6 px-5">
-              <p className="text-center text-base font-medium text-gray-500">
-                Existing customer?{' '}
-                <button type="button" className="text-gray-900 hover:underline">
-                  Login
-                </button>
-              </p>
-            </div>
-          </div>
-        </div>
-      </Popover.Panel>
-    </Transition>
+        </Popover.Panel>
+      </Transition>
+      <SignIn signIn={loginModal} />
+      <SignUp signUp={signUpModal} />
+    </>
   );
 };
 
